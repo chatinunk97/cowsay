8e4fd293bce631e5a7255678b7af1284e2b0229d
chatinunk97 <chatinun.hobby@gmail.com>
2023-09-12 09:01:26 +0700

    Update edit bug

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const chalk = require("chalk");
 const { randomFunction, cowFunction } = require("./utils");
 
-function makeCowSay(count){
-    for (let i = 1; i <= count; i++) {
-        var randomColor = randomFunction
-        console.log(chalk.hex(randomColor)(cowFunction(`This is Cow number ${i}`)));
-      }
-      
+function makeCowSay(count) {
+  for (let i = 1; i <= count; i++) {
+    var randomColor = randomFunction();
+    console.log(chalk.hex(randomColor)(cowFunction(`This is Cow number ${i}`)));
+  }
 }
 
-makeCowSay(5); 
+makeCowSay(5);
